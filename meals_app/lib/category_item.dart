c39461fcad6bd67dff966854b83a7392d000251d
Alexey Zhelezov <zhelezovalexey@gmail.com>
2020-03-24 14:44:59 +0100

    doing meals app

---
+++ meals_app/lib/category_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title, id;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen(id, title);
+    }),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15)
+    );
+  }
+}
